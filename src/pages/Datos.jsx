603db1b4316cb281a22902cdb1b7bc484dbfa8b2
unknown <edgarrios412@gmail.com>
2024-01-22 14:35:31 -0500

    push

--- src/pages/Datos.jsx
+++ src/pages/Datos.jsx
@@ -1,12 +1,27 @@
 import { useParams } from "react-router-dom"
 import NavBar from "../components/NavBar"
+import { useEffect, useState } from "react"
 
 const Datos = () => {
-    const {cedula} = useParams()
+    const { cedula } = useParams()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false),3000)
+    },[])
     return (
         <>
-        <NavBar/>
-            <h1>{cedula}</h1>
+            <NavBar />
+            {loading ? <div className="text-center" style={{marginTop: "200px"}}>
+                <div className="spinner-border text-secondary" role="status">
+                </div><br></br><br></br>
+                <span className="text-secondary">Buscando vigilado<br></br>CC: {cedula}</span>
+            </div>
+            :
+            <div className="text-center" style={{marginTop: "200px"}}>
+                <span className="text-secondary">Vigilado no encontrado<br></br>CC: {cedula}</span>
+            </div>
+            }
         </>
     )
 }
