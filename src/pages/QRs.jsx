cd0b94543927037e9755fb2094a5de3f1e0c9f5f
edgarrios412 <edgarrios412@gmail.com>
2024-04-16 10:06:58 -0500

    remodel tarjeta digital

--- src/pages/QRs.jsx
+++ src/pages/QRs.jsx
@@ -15,7 +15,7 @@ const QRs = () => {
   return (
     <div className="bg-gray-100 h-full absolute w-full font-[OpenSans]">
       {usuario ? (
-        <div className="border-2 h-[520px] w-72 rounded-lg bg-white m-auto mt-32 p-6">
+        <div className="border-2 h-[520px] w-80 rounded-lg bg-white m-auto mt-32 p-6">
           <img
             src="https://f.rpp-noticias.io/2019/02/15/753296descarga-7jpg.jpg"
             className="rounded-full w-24 h-24 bg-green-300 m-auto"
@@ -25,26 +25,30 @@ const QRs = () => {
               {usuario.name} {usuario.lastname}
             </h2>
             <h3 className=" text-gray-400 text-sm">Supervisor de obra</h3>
-            <div className="text-left mt-4 grid grid-cols-2 justify-center">
-              <div className="m-auto">
+            <div className="text-left mt-4 grid grid-cols-2">
+              <div className="ml-6">
                 <h4 className="font-bold text-sm">Documento</h4>
                 <h4 className="mb-4 text-sm">1075650712</h4>
               </div>
-              <div className="m-auto">
+              <div className="">
+                <h4 className="font-bold text-sm">Fecha expedición</h4>
+                <h4 className="mb-4 text-sm">16/04/2024</h4>
+              </div>
+              <div className="ml-6">
                 <h4 className="font-bold text-sm">Teléfono</h4>
                 <h4 className="mb-4 text-sm">3118268264</h4>
               </div>
-              <div className="m-auto">
-                <h4 className="font-bold text-sm">Documento</h4>
-                <h4 className="mb-4 text-sm">1075650712</h4>
-              </div>
-              <div className="m-auto">
-                <h4 className="font-bold text-sm">Documento</h4>
-                <h4 className="mb-4 text-sm">1075650712</h4>
+              <div className="">
+                <h4 className="font-bold text-sm">Fecha caducidad</h4>
+                <h4 className="mb-4 text-sm">16/04/2025</h4>
               </div>
             </div>
+            <div className="m-auto">
+                <h4 className="font-bold text-sm">Número de certficación</h4>
+                <h4 className="mb-4 text-sm">3120756350712</h4>
+              </div>
             <QRCode
-              className="m-auto mt-10"
+              className="m-auto mt-4"
               size={80}
               // style={{ height: "auto", maxWidth: "100%", width: "100%" }}
               value={"https://teldip.com/qr/" + serial}
